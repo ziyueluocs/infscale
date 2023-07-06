f2dd6e9caabefb5d4e0a30285fc424878f74d749
changrong <wcr@live.cn>
2023-07-06 17:42:21 +0000

    add profiling for resnet50 and vgg16

---
+++ profiling/vgg16_profiling.py
@@ -0,0 +1,21 @@
+import torchvision.models as models
+import torch
+from deepspeed.profiling.flops_profiler import get_model_profile
+from deepspeed.accelerator import get_accelerator
+from profile_in_json import get_model_inference_profile
+
+with get_accelerator().device(0):
+    model = models.vgg16()
+    batch_size = 256
+    flops, macs, params = get_model_inference_profile(model=model, # model
+                                    input_shape=(batch_size, 3, 224, 224), # input shape to the model. If specified, the model takes a tensor with this shape as the only positional argument.
+                                    args=None, # list of positional arguments to the model.
+                                    kwargs=None, # dictionary of keyword arguments to the model.
+                                    print_profile=True, # prints the model graph with the measured profile attached to each module
+                                    detailed=True, # print the detailed profile
+                                    module_depth=-1, # depth into the nested modules, with -1 being the inner most modules
+                                    top_modules=1, # the number of top modules to print aggregated profile
+                                    warm_up=10, # the number of warm-ups before measuring the time of each module
+                                    as_string=True, # print raw numbers (e.g. 1000) or as human-readable strings (e.g. 1k)
+                                    output_file="vgg16_profile.json", # path to the output file. If None, the profiler prints to stdout.
+                                    ignore_modules=None) # the list of modules to ignore in the profiling
